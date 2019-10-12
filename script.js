54b580d463984a911ebf26690bcd6f88b1ae0cb7
rupali saini <rupalisni1999@gmail.com>
2019-10-12 18:42:46 -0400

    removed typos

--- script.js
+++ script.js
@@ -6,15 +6,15 @@ var mutationObserver = new MutationObserver(function(mutations) {
     <div class="replacement-div" style="text-align: center; width: 500px; margin: auto">
             <div class="text-warning">
                 <h3>This post was marked as sensitive through the 
-                    <span style="color: #3b5998">content warning</span> extension</h3>
+                    <span style="color: #3b5998">content warning</span> extension.</h3>
             </div>
             <div class="extra-info">
-                <h5>To update your sensitivty settings and preferences, go to the 
-                    <span style="color: #3b5998">content warning</span> extension in your browser</h5>
+                <h5>To update your sensitivity settings and preferences, go to the 
+                    <span style="color: #3b5998">content warning</span> extension in your browser.</h5>
             </div>
         </div>
     `;
-    
+
     chrome.storage.sync.get("warnings", function(a){
       let keywords = [];
       if(a.warnings === undefined){
