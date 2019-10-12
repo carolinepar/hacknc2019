baa88a826503f708a5bed5c9a9eac1a91d541d2a
samauld <samuelaauld@gmail.com>
2019-10-12 18:50:42 -0400

    implemented remove button

--- interfaceScript.js
+++ interfaceScript.js
@@ -1,9 +1,11 @@
-let ar = [];
-chrome.storage.sync.get("warnings", function(a){ar = a.warnings});
-if(ar == undefined){
-    chrome.storage.sync.set({"warnings": []}, function(){console.log("added")});
-}
+chrome.storage.sync.get("warnings", function(data){
+    if(data.warnings === undefined){
+        chrome.storage.sync.set({"warnings": []}, function(){console.log("new storage made")})
+    }
+})
+
 document.getElementById("addButton").onclick = function() {addWarning()};
+document.getElementById("removeButton").onclick = function() {removeWarning()};
 
 function addWarning(){
     let newWarning = document.getElementById("warnings").value;
@@ -12,6 +14,20 @@ function addWarning(){
         warning.push(newWarning);
         chrome.storage.sync.set({"warnings": warning}, function(){console.log("pls work", warning)})
     });
+    document.getElementById("warnings").value = "";
+}
+
+function removeWarning(){
+    let removeWarning = document.getElementById("removals").value;
+    chrome.storage.sync.get("warnings", function(a){
+        let warning = a.warnings;
+        let index = warning.indexOf(removeWarning);
+        if(index > -1) {
+            warning.splice(index, 1);
+        }
+        chrome.storage.sync.set({"warnings": warning}, function(){console.log("pls work", warning)})
+    });
+    document.getElementById("removals").value = "";
 }
 
 document.getElementById("warnings").addEventListener("keyup", function(event) {
