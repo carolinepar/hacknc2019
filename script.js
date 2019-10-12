042b7e1e1f9a4c7c6476969b8e6f9f34d9c92b91
carolinepar <51763164+carolinepar@users.noreply.github.com>
2019-10-12 16:07:50 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,78 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+var x = document.getElementsByClassName("_2s1y");
+//x.style.backgroundColor = "red";
+var i;
+for (i = 0; i < x.length; i++) {
+  x[i].style.backgroundColor = "red";
+}
+var mutationObserver = new MutationObserver(function(mutations) {
+  mutations.forEach(function(mutation) {
+    //let x = document.getElementsByClassName("userContentWrapper");
+    let x = document.querySelectorAll('[data-testid=fbfeed_story]');
+    
+    let i;
 
-'use strict';
+    for(i = 0; i < x.length; i++) {
+      if(typeof(x[i]) !== 'undefined') {
+        let oldNode = x[i];
+        let child = x[i].childNodes[0];
+        //child.style.display = 'none';
+        let replace = document.createElement("div");
+        replace.innerHTML = `<div class="replacement-div"><div class="text-warning"><h1>This post was marked as sensitive through the PLACEHOLDER_EXTENSION_NAME</h1></div><div class="view-check-form"><button class="contentButton" type="button" onclick="display()">Would you like to see this post?</button></div></div>`;
+        x[i].parentElement.replaceChild(replace, x[i]);
+       
+      }
+    }
+    
 
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({color: '#3aa757'}, function() {
-    console.log("The color is green.");
+    function display() {
+      alert('yeet');
+    }
+    /*for (i = 0; i < x.length; i++) {
+    
+
+
+      //let oldNode = x[i];
+      let replace = document.createElement("div");
+      replace.innerHTML = `<div class="replacement-div"><div class="text-warning"><h1>This post was marked as sensitive through the PLACEHOLDER_EXTENSION_NAME</h1></div><div class="view-check-form"><button class="contentButton" type="button" onclick="alert("yeet")">Would you like to see this post?</button></div></div>`;
+    
+      x[i].parentElement.replaceChild(replace, x[i]);*/
+
+      //let parent = x[i].parentElement;
+      //x[i].style.display = "none";
+      //alert(parent.hasChildNodes);
+        
+      //oldNodes[i] = x[i].parentElement.removeChild();
+      //x[i].appendChild(replace);
+      //x[i].parentElement.parentElement.appendChild(replace);
+
+
+      
+      //x[i].parentElement.appendChild(replace);
+
+      //
+            
+      
+     // x[i].parentElement.replaceChild(replace, x[i]);
+    
   });
+});
+/*
+function displayContent (post) {
+    x[i].style.display = "initial";
+}
+*/function display () {
+  alert("yeet");
+}
+
+// Select the node that will be observed for mutations
+const targetNode = document.querySelector('[aria-label="News Feed"]');
+
+// Options for the observer (which mutations to observe)
+mutationObserver.observe(document.documentElement, {
+  attributes: true,
+  characterData: true,
+  childList: true,
+  subtree: true,
+  attributeOldValue: true,
+  characterDataOldValue: true
 });
