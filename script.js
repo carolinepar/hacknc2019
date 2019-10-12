be4c1aa00dbffebab79a4bd3992739335140dc3d
rupali saini <rupalisni1999@gmail.com>
2019-10-12 19:25:39 -0400

    changes in CSS

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var mutationObserver = new MutationObserver(function(mutations) {
     let x = document.getElementsByClassName("userContentWrapper");
     let replace = document.createElement("p");
     replace.innerHTML = `
-    <div class="replacement-div" style="text-align: center; width: 500px; margin: auto">
+    <div class="replacement-div" style="text-align: center; width: auto; margin: auto">
             <div class="text-warning">
                 <h3>This post was marked as sensitive through the 
                     <span style="color: #3b5998">content warning</span> extension</h3>
