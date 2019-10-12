1407b84508834ea36800eae292376528fc032518
rupali saini <rupalisni1999@gmail.com>
2019-10-12 18:27:16 -0400

    storage access from script.js works now

--- script.js
+++ script.js
@@ -3,25 +3,25 @@ var mutationObserver = new MutationObserver(function(mutations) {
     let x = document.getElementsByClassName("userContentWrapper");
     let replace = document.createElement("p");
     replace.innerHTML = "<h1>KEYWORD FOUND AND REPLACED</h1>";
-    let keywords = [];
-    let ar = [];
-    chrome.storage.sync.get("warnings", function(a){ar = a.warnings});
-    //if(ar != undefined){
-      keywords = ar;
-      console.log(keywords);
-    //}
-    for(let i = 0; i < x.length; i++) {
-      for(let j = 0; j < keywords.length; j++) {
-        if(x[i].textContent.includes(keywords[j])) {
-          x[i].parentElement.replaceChild(replace, x[i]);
+    chrome.storage.sync.get("warnings", function(a){
+      let keywords = [];
+      if(a.warnings === undefined){
+        keywords = [];
+        console.log(keywords);
+      } else{
+        keywords = a.warnings;
+        console.log(keywords);
+      }
+      for(let i = 0; i < x.length; i++) {
+        for(let j = 0; j < keywords.length; j++) {
+          if(x[i].textContent.includes(keywords[j])) {
+            x[i].parentElement.replaceChild(replace, x[i]);
+          }
         }
       }
-    }
+    });
   });
 });
-
-// Select the node that will be observed for mutations
-const targetNode = document.querySelector('[aria-label="News Feed"]');
 
 // Options for the observer (which mutations to observe)
 mutationObserver.observe(document.documentElement, {
